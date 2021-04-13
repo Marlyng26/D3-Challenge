73638cf5b891c1411a3146b2bc8cd879cdfdcf97
marlyng <marlyngmorales26@gmail.com>
2021-04-13 11:45:03 -0400

    start

---
+++ StarterCode/assets/js/app.js
@@ -0,0 +1,7 @@
+// Declare variables for x and y
+var XAxis = "poverty";
+var YAxis = "healthcare";
+// Function used for updating x-scale 
+function xScale(data, XAxis, chartWidth) 
+    // Create scales.
+    
